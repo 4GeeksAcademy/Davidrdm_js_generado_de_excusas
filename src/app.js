2c8f9390d43fe293e3de6c57a7db8b89d8593f55
Davidruiz03061976 <davidruizdemanuel39@gmail.com>
2025-02-19 11:21:44 +0000

    firts

--- src/app.js
+++ src/app.js
@@ -1,11 +1,28 @@
-import "bootstrap";
-import "./style.css";
-
-
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  function generateExcuse() {
+      let who = ["Mi hermano", "Mi papa", "El cartero", "Mi suegra"];
+      let action = ["saltando", "comiendo", "conduciendo", "llamando"];
+      let what = ["mis documentos", "mi portatil", "mi furgoneta"];
+      let when = [
+          "por la mañana",
+          "mientras hacia ejercicio",
+          "en la comida",
+          "en una reunion",
+          "mientras iba en barca"
+      ];
+      let excuse = `${who[Math.floor(Math.random() * who.length)]} ${action[Math.floor(Math.random() * action.length)]} ${what[Math.floor(Math.random() * what.length)]} ${when[Math.floor(Math.random() * when.length)]}`;
+      document.getElementById("textoExcusa").innerText = excuse;
+  }
+  generateExcuse();
 };
+
+
+
+
+
+
+
+
+
+
+
