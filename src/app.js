56a0497966ad53e9e89d26b4d744036320656fcc
Davidruiz03061976 <davidruizdemanuel39@gmail.com>
2025-02-19 17:56:47 +0000

    firts

--- src/app.js
+++ src/app.js
@@ -1,28 +1,19 @@
-window.onload = function() {
-  function generateExcuse() {
-      let who = ["Mi hermano", "Mi papa", "El cartero", "Mi suegra"];
-      let action = ["saltando", "comiendo", "conduciendo", "llamando"];
-      let what = ["mis documentos", "mi portatil", "mi furgoneta"];
-      let when = [
-          "por la mañana",
-          "mientras hacia ejercicio",
-          "en la comida",
-          "en una reunion",
-          "mientras iba en barca"
-      ];
-      let excuse = `${who[Math.floor(Math.random() * who.length)]} ${action[Math.floor(Math.random() * action.length)]} ${what[Math.floor(Math.random() * what.length)]} ${when[Math.floor(Math.random() * when.length)]}`;
-      document.getElementById("textoExcusa").innerText = excuse;
-  }
-  generateExcuse();
+window.onload = function () {
+  //array with the words
+  let who = ["Mi Amigo", "Mi padre", "Mi vecino", "El cartero"];
+  let action = ["conducir", "pasear", "nadar", "correr"];
+  let what = ["mi telefono", "mi perro", "mi bicicleta", "mi compra"];
+  let when = ["por la mañana", "en el desayuno", " al atarceder", "en verano"];
+  let where = ["en la carretera", "por la playa", "en el rio", "en el parque"];
+
+  let rdm1 = Math.floor(Math.random() * who.length);
+  let rdm2 = Math.floor(Math.random() * action.length);
+  let rdm3 = Math.floor(Math.random() * what.length);
+  let rdm4 = Math.floor(Math.random() * when.length);
+  let rdm5 = Math.floor(Math.random() * where.length);
+
+  // creating a sentence (the excuse)
+  const excusa = document.querySelector("#excuse");
+
+  excusa.innerHTML = `${who[rdm1]} ${action[rdm2]} ${what[rdm3]} ${when[rdm4]} ${where[rdm5]}`;
 };
-
-
-
-
-
-
-
-
-
-
-
